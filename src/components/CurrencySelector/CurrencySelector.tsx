859c6015873da7102dfaef21b7e3a6f672a087a3
Pavel Klochkov <ckomop0x@users.noreply.github.com>
2024-04-27 19:00:48 +0200

    Extract handlers to CurrencySelector component (#335)

--- src/components/CurrencySelector/CurrencySelector.tsx
+++ src/components/CurrencySelector/CurrencySelector.tsx
@@ -1,31 +1,66 @@
-import DropdownSelector from '../DropdownSelector';
-import { currencyConverter } from '../../helpers';
 import { memo } from 'react';
+
+import DropdownSelector from '../DropdownSelector';
 import CurrencyLine from '../CurrencyLine';
+import { currencyConverter, inputFilter } from '@/helpers';
 
 interface CurrencySelectorProps {
   currencyOne: string;
   currencyOneValue: string;
-  changeCurrencyOneValue: (value: number) => void;
   currencyTwo: string;
   currencyTwoValue: string;
-  changeCurrencyTwoValue: (value: number) => void;
-  handleChangeOne: (currency: string) => void;
-  handleChangeTwo: (currency: string) => void;
   rates: Record<string, number>;
+  setCurrencyOne(value: string): void;
+  setCurrencyTwo(value: string): void;
+  setCurrencyOneValue(value: string): void;
+  setCurrencyTwoValue(value: string): void;
 }
 
 const CurrencySelector = ({
   currencyOne,
   currencyOneValue,
-  changeCurrencyOneValue,
   currencyTwo,
   currencyTwoValue,
-  changeCurrencyTwoValue,
-  handleChangeOne,
-  handleChangeTwo,
+  setCurrencyOne,
+  setCurrencyTwo,
+  setCurrencyOneValue,
+  setCurrencyTwoValue,
   rates,
 }: CurrencySelectorProps) => {
+  const handleChangeTwo = (value: string) => {
+    setCurrencyTwo(value);
+    setCurrencyOneValue('');
+    setCurrencyTwoValue('');
+  };
+
+  const handleChangeOne = (value: string) => {
+    setCurrencyOne(value);
+    setCurrencyOneValue('');
+    setCurrencyTwoValue('');
+  };
+
+  const changeCurrencyTwoValue = (event: any) => {
+    setCurrencyTwoValue(inputFilter(event.target.value));
+    setCurrencyOneValue(
+      currencyConverter(
+        rates[currencyTwo],
+        rates[currencyOne],
+        parseFloat(inputFilter(event.target.value)),
+      ).toString(),
+    );
+  };
+
+  const changeCurrencyOneValue = (event: any) => {
+    setCurrencyOneValue(inputFilter(event.target.value));
+    setCurrencyTwoValue(
+      currencyConverter(
+        rates[currencyOne],
+        rates[currencyTwo],
+        parseFloat(inputFilter(inputFilter(event.target.value))),
+      ).toString(),
+    );
+  };
+
   return (
     <div className="w-auto m-auto text-center flex flex-col items-center content-center content-between gap-4">
       <div className="flex flex-row text-center items-center content-center w-full">
