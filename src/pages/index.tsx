719011af6ecb6d6d590e5eea6732c4d635f3c95d
Pavel Klochkov <ckomop0x@users.noreply.github.com>
2025-02-01 13:54:12 +0100

    Fix and updagrade (#485)

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -2,8 +2,8 @@ import App from '../components/App/App';
 
 const Home = () => (
   <div className="flex justify-center items-center h-screen">
-    <div className="border-solid p-6 rounded-lg shadow-md border border-sky-500 max-w-600 bg-neutral-900">
-      <h1 className="text-xl font-bold">Currency Exchange App</h1>
+    <div className="border-solid p-10 rounded-lg shadow-md border border-sky-500 max-w-600 bg-neutral-900">
+      <h1 className="text-2xl font-bold">Currency Exchange App</h1>
       <App />
     </div>
   </div>
